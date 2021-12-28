aca6c5111d76e619f0ce9e00fd5915ccaba3aeb3
Raulcudris <raulcudriz@hotmail.com>
2021-12-27 22:32:58 -0500

    Archios de Shiny actualizados

--- Dashboard-Estructura/Estructura_Dashboard.R
+++ Dashboard-Estructura/Estructura_Dashboard.R
@@ -88,3 +88,18 @@ ui<-dashboardPage(title = "Dashboard",skin = "green",
                   )
                   
 )
+
+shinyServer(function(input, output) {
+  
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+  
+})
+
+shinyApp(ui = ui, server = server)
+
---
+++ Dashboard-Estructura/barralateral_expandida.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", icon = icon("th"), tabName = "widgets"),
+      menuItem("Charts", icon = icon("bar-chart-o"), startExpanded = TRUE,
+               menuSubItem("Sub-item 1", tabName = "subitem1"),
+               menuSubItem("Sub-item 2", tabName = "subitem2")
+      )
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("dashboard", "Dashboard tab content"),
+      tabItem("widgets", "Widgets tab content"),
+      tabItem("subitem1", "Sub-item 1 tab content"),
+      tabItem("subitem2", "Sub-item 2 tab content")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
